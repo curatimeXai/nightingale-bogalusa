20a22ca2deb5c891400324a1f1b794062a28a651
_Nico_ <132773973+Aqua511@users.noreply.github.com>
2025-10-29 19:38:47 +0100

    Update vue.config.js
    Aws amplify doesnt work with static

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,13 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/static/'
+// })
+
+// Modification for AWS Amplify (doesnt accept static)
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/static/'
+  publicPath: './'
 })
